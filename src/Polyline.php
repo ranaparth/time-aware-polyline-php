2f1aafc960df605c4edba1e32df6e8d327c00647
Rana Amrit Parth <ramrit9@gmail.com>
2017-09-05 18:14:37 +0530

    Adding basic details

---
+++ src/Polyline.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace RanaParth\Polyline;
+
+/**
+*  A sample class
+*
+*  Use this section to define what this class is doing, the PHPDocumentator will use this
+*  to automatically generate an API documentation using this information.
+*
+*  @author https://github.com/ranaparth
+*/
+class TimeAwarePolyine {
+
+}
