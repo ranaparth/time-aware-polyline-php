5ad2be48fcdd28cddc8680aa1ab5753a78da2d4a
Rana Amrit Parth <ramrit9@gmail.com>
2017-09-06 17:53:36 +0530

    Adding test cases

--- tests/TimeAwarePolyineTest.php
+++ tests/TimeAwarePolyineTest.php
@@ -20,8 +20,24 @@ class TimeAwarePolyineTest extends PHPUnit_Framework_TestCase {
 
         $gpxLogs = [
             [19.13626, 72.92506, '2016-07-21T05:43:09+00:00'],
-            [19.13597, 72.92495, '2016-07-21T05:43:15+00:00'],
-            [19.13553, 72.92469, '2016-07-21T05:43:21+00:00'],
+            [19.13597, 72.92495, '2016-07-21T05:43:15+00:00']
+        ];
+
+        $timeAwarePolyline = 'spxsBsdb|Lymo`qvAx@TK';
+
+        $this->assertTrue($var->encode($gpxLogs) == $timeAwarePolyline);
+
+        unset($var);
+
+        $gpxLog = [19.13597, 72.92495, '2016-07-21T05:43:15+00:00'];
+
+        $var = new RanaParth\Polyline\TimeAwarePolyine;
+
+        $var->setPreviousPolyline($timeAwarePolyline);
+        $var->setLastGpxLogs($gpxLog);
+
+        $gpxLogs = [
+            [19.13553, 72.92469, '2016-07-21T05:43:21+00:00']
         ];
 
         $timeAwarePolyline = 'spxsBsdb|Lymo`qvAx@TKvAr@K';
